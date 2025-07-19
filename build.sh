971b1f5ac8707863f46558dee074d68044791d05
algroznykh <alg@0x08.in>
2025-07-19 15:04:08 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ compute.cpp src/glad.c -I/home/suh/opengl_hello_compute/include -I/usr/include/opencv4 -lglfw3 -lpulse -lpulse-simple -lfftw3f -pthread -lopencv_core -lopencv_imgproc -lopencv_videoio
